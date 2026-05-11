04d04d4bc423381c5ef280481a4679f53c835f6d
LunaEnzo875 <enzoluna760@gmail.com>
2026-05-11 14:38:39 -0300

    clases

--- Entidades/RazonDevolucion.cs
+++ Entidades/RazonDevolucion.cs
@@ -1,2 +1,8 @@
 namespace calzados;
 
+public class RazonDevolucion
+{
+    public int idRazon {get; set;}
+    public string Descripcion {get; set;}
+}
+
---
+++ Entidades/Zapatilla.cs
@@ -0,0 +1,9 @@
+namespace namespace calzados;
+
+public class Zapatilla 
+{
+    public int idZapatilla {get; set;}
+    public Modelo modelo {get; set;}
+    public string nombre {get; set;}
+    public Pais pais {get; set;}
+}
