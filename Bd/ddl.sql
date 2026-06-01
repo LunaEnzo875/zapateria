5ce2bade8d47da5699f34eb644d64f79835a174b
LunaEnzo875 <enzoluna760@gmail.com>
2026-06-01 14:50:18 -0300

    Creacuin de bd

---
+++ Bd/ddl.sql
@@ -0,0 +1,31 @@
+SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+CREATE TABLE Cliente(
+    dni int PRIMARY KEY AUTO_INCREMENT,
+    nombre varchar(45) not null,
+    apellido varchar(45) not null,
+    nacimiento DATETIME not null,
+    correo varchar(45) not null
+);
+
+CREATE TABLE Color(
+    idColor int PRIMARY KEY,
+    nombre varchar(45)
+);
+
+CREATE TABLE Compra(
+    numeroCompra int PRIMARY KEY AUTO_INCREMENT,
+    dni int PRIMARY KEY,
+    fechaHora DATETIME,
+    FOREIGN KEY (dni) REFERENCES Cliente (dni)
+);
+
+CREATE TABLE DetalleCompra(
+    numeroCompra int PRIMARY KEY AUTO_INCREMENT,
+    idModelo int NOT NULL,
+    talle int NOT NULL,
+    idZapatilla int NOT NULL,
+    precioUnitario int NOT NULL,
+    cantidad int
+);
