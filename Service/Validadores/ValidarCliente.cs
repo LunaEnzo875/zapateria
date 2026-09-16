86966ad5300f1a573153bba753d71f355e44b26e
LunaEnzo875 <enzoluna760@gmail.com>
2026-09-16 17:54:58 -0300

    Si2

---
+++ Service/Validadores/ValidarCliente.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+using Core.Entidades;
+using Core.Dto;
+using Core.IService;
+using Core.IRepositorio;
+
+namespace Service.Validadores;
+public class ValidarCliente
+{
+        
+}
