9ed4fecaab63f83e35e3d5412761ed65f07cdefe
LunaEnzo875 <enzoluna760@gmail.com>
2026-09-16 17:49:36 -0300

    Si

--- Core/Entidades/Cliente.cs
+++ Core/Entidades/Cliente.cs
@@ -2,18 +2,22 @@ namespace Core.Entidades;
 
 public class Cliente
 {
-    public int idCliente {get; set;}
-    public int dni {get; set;}
-    public string nombre {get; set;}
-    public string apellido {get; set;}
-    public DateOnly nacimiento {get; set;}
-    public string correo {get; set;}
-    
-    public Cliente (int dni,string Nombre, string Apellido, DateOnly Nacimiento)
+    public int idCliente { get; set; }
+    public int dni { get; set; }
+    public string nombre { get; set; } = string.Empty;
+    public string apellido { get; set; } = string.Empty;
+    public DateTime nacimiento { get; set; }
+    public string correo { get; set; } = string.Empty;
+
+    public Cliente()
+    {
+    }
+
+    public Cliente(int dni, string Nombre, string Apellido, DateTime Nacimiento)
     {
         this.dni = dni;
-        nombre = Nombre;
-        apellido = Apellido;
-        nacimiento = Nacimiento;
+        this.nombre = Nombre;
+        this.apellido = Apellido;
+        this.nacimiento = Nacimiento;
     }
 }
